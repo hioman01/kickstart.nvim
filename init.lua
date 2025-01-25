dc09640bc39a7b2a41f42d6fc126f3c65719b68c
hioman01 <hioman01@gmail.com>
2025-01-25 10:35:37 +0800

    feat: add ts and eslint lsp

--- init.lua
+++ init.lua
@@ -697,8 +697,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        ts_ls = {},
+        eslint = {},
 
         lua_ls = {
           -- cmd = { ... },
